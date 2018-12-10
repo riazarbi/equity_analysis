dbbb8061f8918a8bf6c8939a7bb871c2b4503619
Riaz Arbi <riaz.arbi@capetown.gov.za>
2018-12-10 08:03:51 +0200

    1. Added section on docker instructions to paper 2. Added rebalancing periodicity mention to paper 3. Added small section on reporting 4. Added small section on cross-validation 5. Removed parts of the limitations section that have been added to the code 6. Added packages that need to be installed to 0_all.R

--- scripts/0_all.R
+++ scripts/0_all.R
@@ -2,7 +2,9 @@
 list.of.packages <- c("Rblpapi", 
                       "here", 
                       "doParallel", 
-                      "xts")
+                      "xts",
+                      "dygraphs",
+                      "pbo")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
